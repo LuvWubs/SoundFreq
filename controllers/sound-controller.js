a1212fdfa6413249d9da4995d83b08159c084305
LuvWubs <iydkwtmiwt@gmail.com>
2018-05-21 15:23:02 -0600

    server

--- controllers/sound-controller.js
+++ controllers/sound-controller.js
@@ -1,1 +1,9 @@
 // *** all sounds courtesy of http://bbcsfx.acropolis.org.uk/ & https://freesound.org
+
+const express = require("express");
+const router = express.Router();
+const soundFile = require("./models/soundObj.js");
+
+router.get("/", function(req, res) {
+  
+})
--- models/soundObj.js
+++ models/soundObj.js
@@ -1,8 +1,9 @@
+//NOTE does this require need to be here or only the orm requests the soundObj.js???
 var orm = require("./config/orm.js");
 
 var soundFile = {
   getOne: function(id, cb) {
-    var id = "id=" + id;
+    // var id = "id=" + id;
     orm.select("table name", id,  function(res) {
       cb(res);
     });
